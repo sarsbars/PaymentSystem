5470bf643efb233ce80e8c2a7f1818fee4ab616f
Sarah Mitchell <sarahmitchell17@gmail.com>
2025-07-08 09:40:10 -0500

    created abstract class, online payment

---
+++ PaymentSystem/OnlinePayment.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PaymentSystem {
+    // Abstract class representing an online payment
+    public abstract class OnlinePayment : Payment {
+        // Properties
+        public string PaymentGateway { get; private set; }
+
+        // Constructor
+        public OnlinePayment(decimal amount, string currency, string paymentGateway)
+            : base(amount, currency) {
+            PaymentGateway = paymentGateway ?? throw new ArgumentNullException(nameof(paymentGateway));
+        }
+
+        // Methods
+        public abstract void Authorize();
+
+        public override bool ValidatePayment() {
+            if (!base.ValidatePayment()) {
+                return false;
+            }
+            decimal minimum = Currency == "EUR" ? 5.00m : 5.00m;
+            return Amount > minimum;
+        }
+
+        public override void LogPayment() {
+            base.LogPayment();
+            Console.WriteLine($"Payment Gateway: {PaymentGateway}");
+            Console.WriteLine("Payment Mode: Online");
+        }
+    }
+}
