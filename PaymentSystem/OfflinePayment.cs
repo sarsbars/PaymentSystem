15c7fb545413dadcfd35046a583b5f77899466dc
Sarah Mitchell <sarahmitchell17@gmail.com>
2025-07-08 08:12:28 -0500

    created abstract class ,offline payment

---
+++ PaymentSystem/OfflinePayment.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PaymentSystem {
+    // Abstract class representing an offline payment
+    public abstract class OfflinePayment : Payment {
+        // Constructor
+        public OfflinePayment(decimal amount, string currency)
+            : base(amount, currency) {
+        }
+
+        // Methods
+        public abstract void RecordPayment();
+
+        public override bool ValidatePayment() {
+            if (!base.ValidatePayment()) {
+                return false;
+            }
+
+            if (Currency == "EUR") {
+                return false;
+            }
+
+            return true;
+        }
+
+        public override void LogPayment() {
+            base.LogPayment();
+            Console.WriteLine("Payment Mode: Offline");
+        }
+    }
+}
